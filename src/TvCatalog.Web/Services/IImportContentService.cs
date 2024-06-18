eaa1b145d5ebd3d2821f002d4f986a9adf0430a5
Anders Reus <arh@umbraco.dk>
2024-06-18 13:24:51 +0200

    Spring clean

--- src/TvCatalog.Web/Services/IImportContentService.cs
+++ src/TvCatalog.Web/Services/IImportContentService.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TvCatalog.Web.Models.ServiceModels;
+﻿using TvCatalog.Web.Models.ServiceModels;
 
 namespace TvCatalog.Web.Services
 {
